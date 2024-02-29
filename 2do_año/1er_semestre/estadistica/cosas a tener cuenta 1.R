cb9188559af585a6db1b062527e5e9bc0f504100
Tomas-Machin <tm.machinesteve@gmail.com>
2024-02-29 10:52:45 +0100

    estadistica ordenado y comprimido

--- 2do_año/1er_semestre/estadistica/cosas a tener cuenta 1.R
+++
@@ -1,9 +0,0 @@
-demo = function(x){
-  x = 1
-  x
-}
-# java: paso por referencia, lo que cambia dentro, cambia fuera
-# R: paso por copia, lo que cambia dentro, NO cambia fuera
-x = 5
-demo(x)
-print(x)
